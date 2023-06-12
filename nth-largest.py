f71544cf6b5999f4e0e0da9d1b9ed7657cb5b199
nadir-mahmud <97097050+nadir-mahmud@users.noreply.github.com>
2023-06-12 20:09:05 +0600

    Add files via upload

---
+++ nth-largest.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    # List of Integers
+    numbers = [1, 3, 4, 2]
+ 
+    #Sorting list of Integers
+    numbers.sort()
+    return "2nd largest number from the given list is "+ str(numbers[1])
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. You
+    # can configure startup instructions by adding `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
